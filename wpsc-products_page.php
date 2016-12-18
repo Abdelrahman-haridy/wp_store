2d545550c06c59dd950fe993e1fe500f1c3603dc
Abdelrahman-haridy <abdelrahman.haridy01@gmail.com>
2016-12-19 00:39:18 +0200

    steps for install theme

--- wpsc-products_page.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-         <?php /** start the product loop here */?>
- <?php while (wpsc_have_products()) :  wpsc_the_product(); ?>
-
-    <?php the_title(); ?>
-
-    <?php endwhile; ?>
- <?php /** end the product loop here */ ?>
-</div><!--close default_products_page_container-->
